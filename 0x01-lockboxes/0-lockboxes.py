91b5af626e0cf4aba7ff623798c988875d450683
ezana73 <ezana49@gmail.com>
2023-10-04 06:24:09 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
+"""
+
+
 def canUnlockAll(boxes):
     """Determines if boxes can be unlocked"""
     position = 0
